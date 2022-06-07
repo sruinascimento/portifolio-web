ce46a169dece4a188c36575f5e0895d068c76768
sruiNascimento <sruy19@gmail.com>
2022-06-07 15:43:37 -0300

    site desktop refatorado

--- main.js
+++ main.js
@@ -3,40 +3,37 @@ const INTERESSES =  document.querySelectorAll('.img-interesse');
 for (let i = 0; i < INTERESSES.length; i++) {
     INTERESSES[i].onclick = (event) => {
         let idInteresse = INTERESSES[i].id 
-        // console.log(idInteresse);
+        console.log(idInteresse);
         mostraTexto(idInteresse);
     }
 }
 
 const textoInteresses = {
-    'r2d2': 'texto r2d2',
-    'vader': 'texto vader',
-    'stormtrooper': 'texto stoormtrooper',
-    'sabre': 'texto sabre'
+    'r2d2': 'Estudos referentes à temática Tecnologia',
+    'vader': 'Gosto de Star Wars, Série Vikings, Naruto e Dragon Ball Z',
+    'stormtrooper': 'Curto jogos de RPG e MMORPG',
+    'sabre': 'Gosto de viajar e conhecer novas culturas, e principalmente experimentar comidas regionais'
 };
 
 function mostraTexto(idInteresse) {
-    let textoASerMostrado = document.querySelector('#texto-interesse-apresentado');
-    let divAparente = document.querySelector('#display-oculto-amostra');
-    divAparente.classList.remove('display-texto-interesse')
-
-    textoASerMostrado.innerHTML = textoInteresses[idInteresse];
-
+    let conteudoASerMostrado = document.querySelector('#texto-interesse');
+    conteudoASerMostrado.classList.remove('container-interesse-oculto');
+    conteudoASerMostrado.innerHTML = textoInteresses[idInteresse];
 }
 
-const MENU_MOBILE = document.querySelector('#menu-hamburguer');
-const MENU_SAIR = document.querySelector('#menu-exit');
-const NAV_OCULTO = document.querySelector('.nav-itens-mobile');
-const CAIXA_MENU = document.querySelector('.menu-aberto');
+// const MENU_MOBILE = document.querySelector('#menu-hamburguer');
+// const MENU_SAIR = document.querySelector('#menu-exit');
+// const NAV_OCULTO = document.querySelector('.nav-itens-mobile');
+// const CAIXA_MENU = document.querySelector('.menu-aberto');
 
-MENU_MOBILE.onclick = (event) => {
-    NAV_OCULTO.classList.remove('elemento-oculto');
-    MENU_MOBILE.classList.add('elemento-oculto');
-    CAIXA_MENU.classList.remove('elemento-oculto');
-}
+// MENU_MOBILE.onclick = (event) => {
+//     NAV_OCULTO.classList.remove('elemento-oculto');
+//     MENU_MOBILE.classList.add('elemento-oculto');
+//     CAIXA_MENU.classList.remove('elemento-oculto');
+// }
 
-MENU_SAIR.onclick = (event) => {
-    NAV_OCULTO.classList.add('elemento-oculto');
-    MENU_MOBILE.classList.remove('elemento-oculto');
-    CAIXA_MENU.classList.add('elemento-oculto');
-}
+// MENU_SAIR.onclick = (event) => {
+//     NAV_OCULTO.classList.add('elemento-oculto');
+//     MENU_MOBILE.classList.remove('elemento-oculto');
+//     CAIXA_MENU.classList.add('elemento-oculto');
+// }
