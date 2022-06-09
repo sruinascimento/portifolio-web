32ba0680539f572312d0aa16d619563c366f852e
sruiNascimento <sruy19@gmail.com>
2022-06-09 13:09:27 -0300

    site refatorado

--- main.js
+++ main.js
@@ -24,22 +24,15 @@ const MENU_MOBILE = document.querySelector('#menu-hamburguer');
 const MENU_ABERTO = document.querySelector('#menu-aberto');
 const MENU_SAIR = document.querySelector('#menu-exit');
 const PROPRIEDADE_OCULTA = 'elemento-oculto';
-// const NAV_OCULTO = document.querySelector('.nav-itens-mobile');
-// const CAIXA_MENU = document.querySelector('.menu-aberto');
 
 MENU_MOBILE.onclick = (event) => {
     console.log('Menu clicado');
-    // NAV_OCULTO.classList.remove('elemento-oculto');
-    // CAIXA_MENU.classList.remove('elemento-oculto');
     MENU_MOBILE.classList.add(PROPRIEDADE_OCULTA);
     MENU_ABERTO.classList.remove(PROPRIEDADE_OCULTA);
 }
 
 MENU_SAIR.onclick = (event) => {
     console.log('Menu clicado');
-    // NAV_OCULTO.classList.add('elemento-oculto');
-    // MENU_MOBILE.classList.remove('elemento-oculto');
-    // CAIXA_MENU.classList.add('elemento-oculto');
     MENU_MOBILE.classList.remove(PROPRIEDADE_OCULTA);
     MENU_ABERTO.classList.add(PROPRIEDADE_OCULTA);
 }
